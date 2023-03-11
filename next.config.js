764e3950f7b42f764ddea364610d2dd38823bd8a
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-03-11 17:32:06 +0200

    next config fix

--- next.config.js
+++ next.config.js
@@ -1,33 +1,6 @@
-module.exports = withBundleAnalyzer({
-  output: 'standalone',
-  env: {
-    BASE_URL: process.env.BASE_URL,
-  },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = { output: 'standalone' };
 
-  webpack(conf) {
-    conf.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgoConfig: {
-              plugins: [
-                {
-                  // Enable figma's wrong mask-type attribute work
-                  removeRasterImages: false,
-                  removeStyleElement: false,
-                  removeUnknownsAndDefaults: false,
-                  // Enable svgr's svg to fill the size
-                  removeViewBox: false,
-                },
-              ],
-            },
-          },
-        },
-      ],
-    });
-    conf.resolve.modules.push(__dirname);
-    return conf;
-  },
-});
+module.exports = nextConfig;
